13979a57d05f1c06d30507384a90d0c441385104
Krzysiek <krzysztof.woz@gmail.com>
2019-03-16 10:50:27 +0100

    New project created with Spring Initializr

---
+++ src/main/java/io/webskyteam/turbokanban/TurbokanbanApplication.java
@@ -0,0 +1,13 @@
+package io.webskyteam.turbokanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbokanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbokanbanApplication.class, args);
+	}
+
+}
