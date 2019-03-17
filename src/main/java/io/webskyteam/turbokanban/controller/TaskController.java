b96699a2aea610b6f2a20fa508fdac193d9249ce
Krzysiek <krzysztof.woz@gmail.com>
2019-03-17 12:29:07 +0100

    - no db version - first entity class created

--- src/main/java/io/webskyteam/turbokanban/controller/TaskController.java
+++ src/main/java/io/webskyteam/turbokanban/controller/TaskController.java
@@ -1,16 +1,25 @@
 package io.webskyteam.turbokanban.controller;
 
+import io.webskyteam.turbokanban.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TaskController {
 
-    @RequestMapping(value="/table")
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+
+    @RequestMapping("/table")
     public String listTasks() {
+
+
         return "kanban-table";
     }
 }
